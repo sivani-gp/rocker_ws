f95b8fffb674183bc0f7fcc648f6621f7777f2d4
sivani-gp <sivanigandupilli@gmail.com>
2025-06-13 12:26:14 +0530

    ROS2 CONTROL

--- src/rocker_description/launch/gazebo.launch.py
+++ src/rocker_description/launch/gazebo.launch.py
@@ -13,6 +13,9 @@ from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
     bot_description = get_package_share_directory("rocker_description")
+    ros_distro = os.environ["ROS_DISTRO"]
+    is_ignition = "True" if ros_distro == "humble" else "False"
+
 
     model_arg = DeclareLaunchArgument(name="model", default_value=os.path.join(
                                         bot_description, "urdf", "rocker.urdf.xacro"
@@ -29,7 +32,9 @@ def generate_launch_description():
     
     robot_description = ParameterValue(Command([
             "xacro ",
-            LaunchConfiguration("model")
+            LaunchConfiguration("model"),
+            " is_ignition:=",
+            is_ignition
         ]),
         value_type=str
     )
